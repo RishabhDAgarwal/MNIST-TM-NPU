38c64e11ae0eb76893a473eeff75ab6f09b66213
Rishabh Agarwal <sririshi.1010@gmail.com>
2026-07-10 01:19:13 +0530

    Delete relu_activation.v

--- relu_activation.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module relu_activation (
-    input wire signed [63:0] data_in,   
-    output wire signed [63:0] data_out  
-);
-    // If negative output 0. Otherwise pass the 64-bit value 
-    assign data_out = (data_in > 64'sd0) ? data_in : 64'sd0;
-endmodule
